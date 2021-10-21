29d98faa16d7f6fdc1f0cdf3dd083ec3d71a8423
JosePG-98 <92102918+JosePG-98@users.noreply.github.com>
2021-10-21 20:10:25 +0200

    Versió 0.1 del projecte prjava00

---
+++ src/main/java/clsMain.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Jose Perez
+ */
+public class clsMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hola món");
+        System.out.println("Versió 0.1 del projecte prjava00");
+    }
+
+}
